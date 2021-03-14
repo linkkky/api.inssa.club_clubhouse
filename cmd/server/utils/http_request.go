dc26e11078c25161d0c6c98c8f8bd6886a0920bd
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-14 11:11:53 +0900

    feat(utils): Add function responseBodyToString
    responseBodyToString converts io.ReadCloser type of body to string

--- cmd/server/utils/http_request.go
+++ cmd/server/utils/http_request.go
@@ -1,1 +1,13 @@
 package utils
+
+import (
+	"io"
+	"io/ioutil"
+)
+
+func responseBodyToString(body io.ReadCloser) string {
+	defer body.Close()
+	bytes, _ := ioutil.ReadAll(body)
+	strBody := string(bytes)
+	return strBody
+}
