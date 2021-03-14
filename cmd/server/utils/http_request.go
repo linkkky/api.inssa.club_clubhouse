3e5a968cb123c1ba345813c8a3d66766a5625b70
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-14 11:17:15 +0900

    feat(utils): Add function mapToBody
    mapToBody converts map[string]interface{} type of mapBody to *bytes.Buffer, for convenient use of http request

--- cmd/server/utils/http_request.go
+++ cmd/server/utils/http_request.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"io/ioutil"
@@ -20,4 +21,9 @@ func responseBodyToMap(body io.ReadCloser) (map[string]interface{}, error) {
 		return nil, err
 	}
 	return resp, nil
+}
+
+func mapToBody(mapBody map[string]interface{}) *bytes.Buffer {
+	marshaledBody, _ := json.Marshal(mapBody)
+	return bytes.NewBuffer(marshaledBody)
 }
