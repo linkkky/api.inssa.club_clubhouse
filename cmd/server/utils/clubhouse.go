5221a282b4518c6885ad6a570e25396ce1c1b7a2
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-14 11:17:20 +0900

    feat(utils): Add clubhouse request helper functions
    function request for unauthorized request, function authenticatedRequest for authorized request

--- cmd/server/utils/clubhouse.go
+++ cmd/server/utils/clubhouse.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"net/http"
+)
+
 const API_URL = "https://www.clubhouseapi.com/api"
 
 type Clubhouse struct {
@@ -8,4 +12,33 @@ type Clubhouse struct {
 	authToken             string
 	HEADERS               map[string]string
 	AUTHENTICATED_HEADERS map[string]string
+}
+
+func (clubhouse Clubhouse) request(req *http.Request) (map[string]interface{}, error) {
+	client := &http.Client{}
+	for key, value := range clubhouse.HEADERS {
+		req.Header.Add(key, value)
+	}
+	// add Headers
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return responseBodyToMap(resp.Body)
+}
+
+func (clubhouse Clubhouse) authenticatedRequest(req *http.Request) (map[string]interface{}, error) {
+	client := &http.Client{}
+	for key, value := range clubhouse.HEADERS {
+		req.Header.Add(key, value)
+	}
+	for key, value := range clubhouse.AUTHENTICATED_HEADERS {
+		req.Header.Add(key, value)
+	}
+	// add Headers
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return responseBodyToMap(resp.Body)
 }
