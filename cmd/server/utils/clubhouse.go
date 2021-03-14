ae682d32ea986bbac2139ec076046bf84e5e9c21
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-14 11:17:18 +0900

    feat(utils): Add struct Clubhouse

--- cmd/server/utils/clubhouse.go
+++ cmd/server/utils/clubhouse.go
@@ -1,1 +1,11 @@
 package utils
+
+const API_URL = "https://www.clubhouseapi.com/api"
+
+type Clubhouse struct {
+	uuid                  string
+	userID                int
+	authToken             string
+	HEADERS               map[string]string
+	AUTHENTICATED_HEADERS map[string]string
+}
