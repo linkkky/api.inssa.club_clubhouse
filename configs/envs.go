2a01600162f91ff24eb57385d7c3baa7b79d2133
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-15 15:17:19 +0900

    refactor(configs): Rename directory config to configs

--- configs/envs.go
+++ configs/envs.go
@@ -1,6 +1,8 @@
-package config
+package configs
 
 import (
+	"os"
+
 	_ "github.com/joho/godotenv/autoload"
 )
 
@@ -9,12 +11,11 @@ func ternaryMap(value string, defaultValue string) string {
 }
 
 func getEnv(envName string, defaultValue string) string {
-	return ternaryMap(envName, defaultValue)
+	return ternaryMap(os.Getenv(envName), defaultValue)
 }
 
 // Envs has values for environment variables and the defaults for them
 var Envs = map[string]string{
-	"SERVER_PORT":                  getEnv("SERVER_PORT", "3000"),
 	"CLUBHOUSE_ACCOUNT_UUID":       getEnv("CLUBHOUSE_ACCOUNT_UUID", "E656C404-FC76-43F1-816D-2C4F15C4931E"),
 	"CLUBHOUSE_ACCOUNT_USER_ID":    getEnv("CLUBHOUSE_ACCOUNT_USER_ID", "1234"),
 	"CLUBHOUSE_ACCOUNT_AUTH_TOKEN": getEnv("CLUBHOUSE_ACCOUNT_AUTH_TOKEN", "24204e84df89ba376adf588e3a045c326e6023c2"),
