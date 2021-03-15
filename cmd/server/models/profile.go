33dbe6512e55b2945dafa2437aaa15528e3b0d00
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-16 01:01:51 +0900

    feat(models): Add struct ClubhouseProfile

--- cmd/server/models/profile.go
+++ cmd/server/models/profile.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+	"github.com/kamva/mgm/v3"
+)
+
+// ClubhouseProfile is a profile of clubhouse
+type ClubhouseProfile struct {
+	mgm.DefaultModel `bson:",inline"`
+	UserID           uint64                 `json:"user_id" bson:"user_id"`
+	Profile          map[string]interface{} `json:"profile" bson:"profile"`
+}
