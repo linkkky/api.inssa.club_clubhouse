6592018cd243a32847265cec1efcf61d52876c10
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-15 10:57:25 +0900

    feat(config): Add environment values for the Clubhouse account

--- config/envs.go
+++ config/envs.go
@@ -11,3 +11,10 @@ func ternaryMap(value string, defaultValue string) string {
 func getEnv(envName string, defaultValue string) string {
 	return ternaryMap(envName, defaultValue)
 }
+
+// Envs has values for environment variables and the defaults for them
+var Envs = map[string]string{
+	"CLUBHOUSE_ACCOUNT_UUID":       getEnv("CLUBHOUSE_ACCOUNT_UUID", "E656C404-FC76-43F1-816D-2C4F15C4931E"),
+	"CLUBHOUSE_ACCOUNT_USER_ID":    getEnv("CLUBHOUSE_ACCOUNT_USER_ID", "1234"),
+	"CLUBHOUSE_ACCOUNT_AUTH_TOKEN": getEnv("CLUBHOUSE_ACCOUNT_AUTH_TOKEN", "24204e84df89ba376adf588e3a045c326e6023c2"),
+}
