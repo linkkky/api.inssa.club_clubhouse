99e4ea6d8ae6800f0d07dd5fb34752eb56b838ce
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-16 01:05:03 +0900

    feat(models): Add constructor for ClubhouseProfile as function NewClubhouseProfile

--- cmd/server/models/profile.go
+++ cmd/server/models/profile.go
@@ -10,3 +10,18 @@ type ClubhouseProfile struct {
 	UserID           uint64                 `json:"user_id" bson:"user_id"`
 	Profile          map[string]interface{} `json:"profile" bson:"profile"`
 }
+
+func profileCleaner(profile map[string]interface{}, fields []string) {
+	for _, field := range fields {
+		delete(profile, field)
+	}
+}
+
+// NewClubhouseProfile is a constructor for ClubhouseProfile
+func NewClubhouseProfile(profile map[string]interface{}) *ClubhouseProfile {
+	clubhouseProfile := ClubhouseProfile{}
+	clubhouseProfile.UserID = uint64(profile["user_id"].(float64))
+	profileCleaner(profile, []string{"user_id", "follows_me", "invited_by_club", "is_blocked_by_network", "mutual_follows", "mutual_follows_count", "notification_type"})
+	clubhouseProfile.Profile = profile
+	return &clubhouseProfile
+}
