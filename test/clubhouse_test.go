4e367e21cd7cb8ea9f34230dc6d67467e1df4963
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-15 18:18:10 +0900

    test: Add function setupClubhouseProperly

--- test/clubhouse_test.go
+++ test/clubhouse_test.go
@@ -1,1 +1,23 @@
 package test
+
+import (
+	"inssa_club_clubhouse_backend/cmd/server/utils"
+	"inssa_club_clubhouse_backend/configs"
+	"strconv"
+)
+
+const notExistingUsername = "notexistinguser!@#$%^&*()(*&^%$#@!"
+
+var clubhouse = utils.SingletonClubhouse()
+
+func setupClubhouseProperly() {
+	UUID := configs.Envs["CLUBHOUSE_ACCOUNT_UUID"]
+	USER_ID, err := strconv.Atoi(configs.Envs["CLUBHOUSE_ACCOUNT_USER_ID"])
+	if err != nil {
+		panic(err)
+	}
+	AUTH_TOKEN := configs.Envs["CLUBHOUSE_ACCOUNT_AUTH_TOKEN"]
+	utils.SingletonClubhouse().SetAccount(UUID, USER_ID, AUTH_TOKEN)
+}
+
+// tools for convenient test
