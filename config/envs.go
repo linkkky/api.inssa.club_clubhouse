38842140abd06f6fc66a9448bece93dfdc52465c
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-15 10:56:52 +0900

    feat(config): Add functions to get environments easily
    The functions are ternaryMap and getEnv

--- config/envs.go
+++ config/envs.go
@@ -3,3 +3,11 @@ package config
 import (
 	_ "github.com/joho/godotenv/autoload"
 )
+
+func ternaryMap(value string, defaultValue string) string {
+	return map[bool]string{true: value, false: defaultValue}[len(value) != 0]
+}
+
+func getEnv(envName string, defaultValue string) string {
+	return ternaryMap(envName, defaultValue)
+}
