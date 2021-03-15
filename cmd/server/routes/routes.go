7ccbb2728d9e82f87707da4247b4f6256861face
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-16 01:09:54 +0900

    feat(routes): Add struct RouteInfo

--- cmd/server/routes/routes.go
+++ cmd/server/routes/routes.go
@@ -1,1 +1,12 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// RouteInfo is a struct for a route
+type RouteInfo struct {
+	Method  string
+	Path    string
+	Handler gin.HandlerFunc
+}
