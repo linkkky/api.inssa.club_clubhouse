1e7a5f04237fc59b2caeefaba9322498ab6abd4f
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-15 15:18:48 +0900

    feat(utils): Update function clubhouse.authentictedRequest to wrap clubhouse.request

--- cmd/server/utils/clubhouse.go
+++ cmd/server/utils/clubhouse.go
@@ -39,19 +39,11 @@ func (clubhouse Clubhouse) request(req *http.Request) (map[string]interface{}, e
 }
 
 func (clubhouse Clubhouse) authenticatedRequest(req *http.Request) (map[string]interface{}, error) {
-	client := &http.Client{}
-	for key, value := range clubhouse.HEADERS {
-		req.Header.Add(key, value)
-	}
 	for key, value := range clubhouse.AUTHENTICATED_HEADERS {
 		req.Header.Add(key, value)
 	}
 	// add Headers
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	return responseBodyToMap(resp.Body)
+	return clubhouse.request(req)
 }
 
 func NewClubhouse(uuid string, userID int, authToken string) *Clubhouse {
