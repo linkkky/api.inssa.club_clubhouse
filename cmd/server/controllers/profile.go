cb999ea1b43292373b3253f86a3dae2862b92e0c
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-16 01:08:05 +0900

    feat(controllers): Add function getProfileFromDB

--- cmd/server/controllers/profile.go
+++ cmd/server/controllers/profile.go
@@ -1,1 +1,21 @@
 package controllers
+
+import (
+	"fmt"
+	"inssa_club_clubhouse_backend/cmd/server/models"
+
+	"github.com/kamva/mgm/v3"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func getProfileFromDB(username string) (models.ClubhouseProfile, error) {
+	result := []models.ClubhouseProfile{}
+	err := mgm.Coll(&models.ClubhouseProfile{}).SimpleFind(&result, bson.M{"username": username})
+	if err != nil {
+		return models.ClubhouseProfile{}, err
+	}
+	if len(result) == 0 {
+		return models.ClubhouseProfile{}, fmt.Errorf("No such user")
+	}
+	return result[0], nil
+}
